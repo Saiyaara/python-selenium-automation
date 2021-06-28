6b56dd0f48fb7ee3def7f771396c39add6824b3f
Saiyaara <86168196+Saiyaara@users.noreply.github.com>
2021-06-28 11:17:09 -0400

    Add files via upload
    Hi Lana! Trying to correct it as suggested but it returned new error: Webdriver object has no attribute window_handle.

--- Window handling_test case.py
+++ Window handling_test case.py
@@ -5,42 +5,37 @@ from selenium.webdriver.support.wait import WebDriverWait
 from time import sleep
 from selenium.webdriver.support import expected_conditions as EC
 
-@given("Open Amazon T&C 508088 page")
+@given("Open Amazon T&C 508008 page")
 def amazon_page (context):
+    context.original_window = context.driver.get("https://www.amazon.com/gp/help/customer/display.html/ref=ap_register_notification_condition_of_use?ie=UTF8&nodeId=508088")
+
+
+@when("Store original window")
+def store_window(context):
     context.original_window = context.driver.current_window_handle
-
-    sleep(1)
-
-@when("Store original windows")
-def store_windows(context):
-    context.original_window = context.driver.current_window_handle
-
-    sleep(1)
 
 
 @when("Click on Amazon Privacy Notice link")
 def click_link(context):
- context.driver.find_element(By.XPATH,"//a[@href='https://www.amazon.com/privacy']").click()
+ context.driver.find_element(By.XPATH, "//a[@href='https://www.amazon.com/privacy']").click()
 
-sleep(1)
 
 @when("Switch to the newly opened window")
 def switch_to_new_window(context):
     context.driver.wait.until(EC.new_window_is_opened)
     new_window= context.driver.window_handle[1]
     context.driver.switch_to.window(new_window)
-    sleep(1)
+
 
 @then("Verify Amazon Privacy Notice Page is opened")
 def privacy_page(context):
     assert "https://www.amazon.com/gp/help/customer/"in context.driver_url, f"url ttps://www.amazon.com/gp/help/customer/ not in {context.driver}"
 
-    sleep(1)
+
 
 @when("User can close new window")
 def close_window(context):
 
-    sleep(1)
     context.driver.close()
 
 @when("switch back to original")
@@ -48,4 +43,3 @@ def switch_to_original_window(context):
    original_window= context.driver.window_handle[1]
    context.driver.switch_to_original_window(original_window)
 
-sleep(1)
